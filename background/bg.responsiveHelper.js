764d862eb7ac768c63bf4a0a7ae724f1624515e9
Clowst <bon.kevin@gmail.com>
2017-06-25 20:24:48 -0400

    New feature added: Give the choice to show or not an icon if bootstrap is not detected

--- background/bg.responsiveHelper.js
+++ background/bg.responsiveHelper.js
@@ -1,9 +1,35 @@
-const TITLE = "Bootstrap - Responsible Helper";
+const TITLE = 'Bootstrap - Responsible Helper';
+const defaultSettings = {
+  hideIconWhenNonActive: true,
+};
+
+function onError(e) {
+  console.error(e);
+}
+
+function checkStoredSettings(storedSettings) {
+  return new Promise((resolve, reject) => {
+    if (!Object.keys(storedSettings).length) {
+      // Set settings by default
+      browser.storage.local.set(defaultSettings)
+        .then(() => resolve(defaultSettings))
+        .catch(reject);
+      return;
+    }
+    resolve(storedSettings);
+  });
+}
 
 function updateIcon(params) {
   let icon = null;
+  let title = TITLE;
   if (!params.bootstrapActive) {
-    return;
+    if (params.storedSettings.hideIconWhenNonActive) {
+      browser.pageAction.hide(params.tabId);
+      return;
+    }
+    icon = '/icons/non-active.png';
+    title += ' (Bootstrap is not detected)'
   } else if (params.viewportWidth < 768) {
     icon = '/icons/size_xs.svg';
   } else if (params.viewportWidth >= 1200) {
@@ -16,16 +42,26 @@ function updateIcon(params) {
     return;
   }
   browser.pageAction.show(params.tabId);
-  browser.pageAction.setIcon({tabId: params.tabId, path: icon });
-  browser.pageAction.setTitle({tabId: params.tabId, title: TITLE });
+  browser.pageAction.setIcon({
+    tabId: params.tabId,
+    path: icon
+  });
+  browser.pageAction.setTitle({
+    tabId: params.tabId,
+    title: title
+  });
 }
 
 function onContentScriptNotification(message, context) {
-  updateIcon({
-    bootstrapActive: message.bootstrapActive,
-    viewportWidth: message.viewportWidth,
-    tabId: context.tab.id
-  });
+  browser.storage.local.get()
+    .then(checkStoredSettings)
+    .then((storedSettings) => updateIcon({
+      bootstrapActive: message.bootstrapActive,
+      viewportWidth: message.viewportWidth,
+      tabId: context.tab.id,
+      storedSettings: storedSettings
+    }))
+    .catch(onError);
 }
 
 browser.runtime.onMessage.addListener(onContentScriptNotification);
